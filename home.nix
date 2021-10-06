12c9b1607adea449d29fd5ea9295ca2083de2ea8
Christopher Grainger <chris@amplified.ai>
2021-10-07 08:59:48 +1100

    split out neovim config

--- home.nix
+++ home.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }:
 
 {
+  imports = [ ./home/neovim.nix ];
+
   home.packages = with pkgs; [
     _1password-gui
     aws-vault
@@ -135,61 +137,6 @@
       config = { theme = "Dracula"; };
     };
 
-    neovim = {
-      enable = true;
-      viAlias = true;
-      vimAlias = true;
-      vimdiffAlias = true;
-      extraConfig = "lua require('init')";
-      extraPackages = with pkgs; [
-        neovim-remote
-        gcc
-        tree-sitter
-        nodePackages.pyright
-        nodePackages.bash-language-server
-        nodePackages.typescript-language-server
-        nodePackages.vscode-json-languageserver
-        nodePackages.yaml-language-server
-        rnix-lsp
-        sumneko-lua-language-server
-        fzf
-        ripgrep
-        elixir_ls
-      ];
-      plugins = with pkgs.vimPlugins; [
-        dracula-vim
-        editorconfig-vim
-        formatter-nvim
-        friendly-snippets
-        gitsigns-nvim
-        lsp-colors-nvim
-        lsp_signature-nvim
-        lualine-nvim
-        nvim-compe
-        nvim-lspconfig
-        nvim-tree-lua
-        nvim-treesitter
-        nvim-web-devicons
-        rust-tools-nvim
-        todo-comments-nvim
-        vim-commentary
-        vim-dadbod
-        vim-fugitive
-        vim-rooter
-        vim-slime
-        vim-speeddating
-        vim-surround
-        vim-terraform
-        vim-test
-        vim-tmux-navigator
-        vim-vsnip
-        which-key-nvim
-        fzf-lsp-nvim
-        fzf-vim
-        vim-nix
-        vim-elixir
-      ];
-    };
 
     tmux = {
       enable = true;
---
+++ home/neovim.nix
@@ -0,0 +1,60 @@
+{ pkgs, ... }:
+
+{
+    programs.neovim = {
+      enable = true;
+      viAlias = true;
+      vimAlias = true;
+      vimdiffAlias = true;
+      extraConfig = "lua require('init')";
+      extraPackages = with pkgs; [
+        neovim-remote
+        gcc
+        tree-sitter
+        nodePackages.pyright
+        nodePackages.bash-language-server
+        nodePackages.typescript-language-server
+        nodePackages.vscode-json-languageserver
+        nodePackages.yaml-language-server
+        rnix-lsp
+        sumneko-lua-language-server
+        fzf
+        ripgrep
+        elixir_ls
+      ];
+      plugins = with pkgs.vimPlugins; [
+        dracula-vim
+        editorconfig-vim
+        formatter-nvim
+        friendly-snippets
+        gitsigns-nvim
+        lsp-colors-nvim
+        lsp_signature-nvim
+        lualine-nvim
+        nvim-compe
+        nvim-lspconfig
+        nvim-tree-lua
+        nvim-treesitter
+        nvim-web-devicons
+        rust-tools-nvim
+        todo-comments-nvim
+        vim-commentary
+        vim-dadbod
+        vim-fugitive
+        vim-rooter
+        vim-slime
+        vim-speeddating
+        vim-surround
+        vim-terraform
+        vim-test
+        vim-tmux-navigator
+        vim-vsnip
+        which-key-nvim
+        fzf-lsp-nvim
+        fzf-vim
+        vim-nix
+        vim-elixir
+      ];
+    };
+
+}
